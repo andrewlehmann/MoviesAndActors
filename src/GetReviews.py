de23f4f0073a339e9cb8776bd7218ea98c3d24e5
Cale Elder <thenameiscale@gmail.com>
2017-12-28 21:14:37 -0600

    added get reviews

--- src/GetReviews.py
+++ src/GetReviews.py
@@ -1,11 +1,14 @@
 import json, requests
 
-url = "http://www.omdbapi.com/?apikey=84585783&"
-
-parameters = {
+def reviewScore(movie_name):
+  url = "http://www.omdbapi.com/?apikey=84585783&"
+  parameters = {
     "type": "Movie",
-    "t": "Die Hard"
-}
-
-response = requests.get(url=url, params=parameters)
-data = json.loads(response.text)
+    "t": movie_name
+  }
+  response = requests.get(url=url, params=parameters)
+  data = json.loads(response.text)
+  review = data['imdbRating']
+  return review
+  
+print(reviewScore("Die Hard"))
